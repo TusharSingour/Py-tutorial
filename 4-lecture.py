b4afa65cadbf35fcbf30dbe715d0bca120c9144a
TusharSingour <tusharsingour.tech@gmail.com>
2025-12-07 10:20:58 +0530

    add 4-lecture

---
+++ 4-lecture.py
@@ -0,0 +1,165 @@
+# dictionary in python 
+
+# info = { 
+#     "name" : "tushar",
+#     "class" : "12th",
+#     "add" : "bhopal"
+# }
+
+# print(info["name"])
+
+# info["name"] = "poonam"
+
+# print(info)
+
+#null.dict
+# null_dict = {}
+# null_dict["name"] = "apnacollege"
+# print(null_dict)
+
+#nested dictinories 
+
+# student = {
+# "name" : "tushar",
+# "score": {
+#     "chem": 99,
+#     "phy": 77,
+#     "math": 95,
+# }
+# }
+
+# print(student)
+# print(student["score"]["chem"])
+
+
+#dictinary methods
+
+#mydict.keys()
+
+# studen = { 
+#     "name" : "tushar",
+#     "studen" : "maths",
+#     "addresh" : "bhopal",
+#     "collage" : "sage univershity",
+# }
+
+# print(subject.keys())
+
+#list me convert
+# print(list(subject.keys()))
+
+#mydict.values()
+
+# studen = { 
+#     "name" : "tushar",
+#     "subject" : "maths",
+#     "addresh" : "bhopal",
+#     "collage" : "sage univershity",
+# }
+
+# print(studen.values())
+
+#mydict.items()
+# student= { 
+#     "name" : "tushar",
+#     "subject" : "maths",
+#     "addresh" : "bhopal",
+#     "collage" : "sage univershity",
+# }
+
+# print(studen.items())
+
+#mydict.get("key")
+
+# student = { 
+#     "name" : "tushar",
+#     "subject" : "maths",
+#     "addresh" : "bhopal",
+#     "collage" : "sage univershity",
+# }
+
+# print(student.get("name"))
+
+# student = { 
+#     "name" : "tushar",
+#     "subject" : "maths",
+#     "addresh" : "bhopal",
+#     "collage" : "sage univershity",  
+# } 
+
+# student.update({"location" : "delhi"})
+# print(student)
+
+
+
+#set in python 
+
+# collection = {1 ,2 ,3 ,4 ,}
+
+# print(collection)
+# print(type(collection))
+# print(len(collection))
+
+#set methods
+
+#set.add()
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(13)
+# collection.add(4)
+
+
+# print(collection)
+
+#set.remove
+
+#set.clear
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(13)
+# collection.add(4)
+
+# collection.clear()
+
+# print(len(collection))
+
+#set.pop
+
+# s
+
+
+#set.union(set2)
+
+# set1 = {1, 2, 3,}
+# set2 = {2, 3, 4,}
+
+# print(set1.union(set2))
+
+#set.intersection(set2)
+
+# set1 = {1, 2, 3,}
+# set2 = {2, 3, 4,}
+
+# print(set1.intersection(set2))
+
+# #Q1 
+
+# dictionary = {
+#     "cat" : "a small animal",
+#     "table" : ["a place of frunction","list of facts & figures"]
+# }
+
+# print(dictionary)
+
+#Q2
+
+# subject = {
+#     "python", "java", "c++", "python","javascript","java","python","java","c++","c"
+# }
+
+# print(len(subject))
+
